9d6d7255244467a182f616dbb1f58a406d0c76dd
RHReynolds <regina.reynolds.16@ucl.ac.uk>
2021-06-16 17:24:55 +0100

    Initial commit

---
+++ scripts/01a_get_test_loci.R
@@ -0,0 +1,177 @@
+# Load packages -----------------------------------------------------------
+
+library(data.table)
+library(GenomicRanges)
+library(qdapTools)
+library(stringr)
+library(tidyverse)
+
+# Load data ---------------------------------------------------------------
+
+ld_blocks <- 
+  read_delim("https://github.com/cadeleeuw/lava-partitioning/raw/main/LAVA_s2500_m25_f1_w200.blocks", 
+             delim = "\t")
+
+gwas_list <- 
+  setNames(
+    object = list(
+      fread("/data/LDScore/GWAS/AD2019/AD_sumstats_Jansenetal_2019sept.lava.gz"),
+      fread("/data/LDScore/GWAS/LBD2020/LBD2020_hg19_rsids.lava.gz"),
+      fread("/data/LDScore/GWAS/PD2019_meta5_ex23andMe/PD2019_ex23andMe_hg19.lava.gz"),
+      fread("/data/LDScore/GWAS/BIP2021/BIP2021.lava.gz"),
+      fread("/data/LDScore/GWAS/MDD2019_ex23andMe/MDD2019.lava.gz"),
+      fread("/data/LDScore/GWAS/SCZ2018/SCZ2018.lava.gz")
+    ),
+    nm = c("ad", "lbd", "pd", "bip", "mdd", "scz")
+  )
+
+# Main --------------------------------------------------------------------
+
+# Filter for only genome-wide significant loci and convert to granges
+gr_list <- 
+  gwas_list %>%
+  lapply(., function(gwas){
+    gwas %>% 
+      dplyr::filter(P < 5e-8) %>% 
+      GenomicRanges::makeGRangesFromDataFrame(
+        .,
+        keep.extra.columns = TRUE,
+        ignore.strand = TRUE,
+        seqinfo = NULL,
+        seqnames.field = "CHR",
+        start.field = "BP",
+        end.field = "BP"
+      )
+  })
+
+# Add locus id and rename remaining columns to fit LAVA requirements
+ld_blocks <- 
+  ld_blocks %>%
+  dplyr::rename_with(
+    .fn = stringr::str_to_upper,
+    .cols = everything()
+  ) %>% 
+  dplyr::mutate(
+    LOC = dplyr::row_number()
+  ) %>% 
+  dplyr::select(LOC, everything())
+
+# Convert to granges
+ld_blocks_gr <-
+  ld_blocks %>%
+  GenomicRanges::makeGRangesFromDataFrame(
+    .,
+    keep.extra.columns = TRUE,
+    ignore.strand = TRUE,
+    seqinfo = NULL,
+    seqnames.field = "chr",
+    start.field = "start",
+    end.field = "stop"
+  ) 
+
+# Overlap granges objects
+overlap_list <- 
+  gr_list %>% 
+  lapply(., function(gr){
+    GenomicRanges::findOverlaps(gr, ld_blocks_gr, type = "within") %>% 
+      as_tibble()
+  })
+  
+
+# Extract relevant rows from ld_blocks using overlap indices
+# Rename remaining columns to fit with LAVA requirements
+loci <- 
+  ld_blocks %>% 
+  dplyr::slice(
+    overlap_list %>% 
+      qdapTools::list_df2df(col1 = "gwas") %>% 
+      .[["subjectHits"]] %>% 
+      unique()
+    ) %>% 
+  dplyr::arrange(LOC)
+  
+# Generate df of GWAS loci that overlap which LD blocks
+overlap_df_list <- vector(mode = "list", length = length(gr_list))
+
+for(i in 1:length(gr_list)){
+  
+  gr <- gr_list[[i]]
+  
+  names(overlap_df_list)[i] <- names(gr_list)[i]
+  
+  if(names(gr_list)[i] == c("mdd")){
+    
+    overlap_df_list[[i]] <- 
+      gr %>% 
+      as_tibble() %>% 
+      dplyr::mutate(
+        BETA = NA,
+        OR = NA
+      ) %>% 
+      dplyr::select(seqnames, start, end, SNP, A1, A2, MAF, BETA, OR, logOdds, SE, P, N) 
+    
+    
+  } else if(names(gr_list)[i] == c("scz")){
+    
+    overlap_df_list[[i]] <- 
+      gr %>% 
+      as_tibble() %>% 
+      dplyr::mutate(
+        BETA = NA,
+        logOdds = NA
+      ) %>% 
+      dplyr::select(seqnames, start, end, SNP, A1, A2, MAF, BETA, OR, logOdds, SE, P, N)
+    
+  } else{
+    
+    overlap_df_list[[i]] <- 
+      gr %>% 
+      as_tibble() %>% 
+      dplyr::mutate(
+        OR = NA,
+        logOdds = NA
+      ) %>% 
+      dplyr::select(seqnames, start, end, SNP, A1, A2, MAF, BETA, OR, logOdds, SE, P, N) 
+    
+  }
+  
+  overlap_df_list[[i]] <-
+    overlap_df_list[[i]] %>% 
+    dplyr::rename_with(
+      ~ stringr::str_c("GWAS", .x, sep = "_")
+    ) %>% 
+    dplyr::slice(overlap_list[[i]]$queryHits) %>% 
+    dplyr::bind_cols(
+      ld_blocks_gr %>% 
+        as_tibble() %>% 
+        dplyr::rename_with(
+          ~ stringr::str_c("LD", .x, sep = "_")
+        ) %>% 
+        dplyr::slice(overlap_list[[i]]$subjectHits)
+    ) %>% 
+    dplyr::select(-contains("strand")) %>% 
+    dplyr::rename_with(
+      ~ stringr::str_replace(.x,
+                             pattern = "seqnames", 
+                             replacement = "CHR"),
+      .col = dplyr::ends_with("seqnames"))
+  
+}
+
+overlap_df <-
+  overlap_df_list %>% 
+  qdapTools::list_df2df(col1= "GWAS")
+
+# Save data ---------------------------------------------------------------
+
+out_dir <- here::here("results", "01_input_prep")
+dir.create(out_dir, showWarnings = T)
+write_delim(
+  loci, 
+  delim = "\t", 
+  file = file.path(out_dir, "gwas_filtered.loci")
+  )
+saveRDS(
+  overlap_df, 
+  file = file.path(out_dir, "gwas_filtered_loci.rds")
+  )
---
+++ scripts/01b_get_sample_overlaps.R
@@ -0,0 +1,170 @@
+# Load packages -----------------------------------------------------------
+
+library(tidyverse)
+library(stringr)
+library(qdapTools)
+
+# Set arguments -----------------------------------------------------------
+
+gwas_dir <- "/data/LDScore/GWAS/"
+
+phenotypes <- c("AD2019", "LBD2020", "PD2019.meta5.ex23andMe", "BIP2021", "MDD2019", "SCZ2018") %>% sort()
+
+out_dir <- here::here("results", "01_input_prep", "ldsc_corr")
+dir.create(out_dir, showWarnings = T)
+
+ldsc_args <- 
+  list(
+    ldsc = "/tools/ldsc/ldsc.py",
+    ref_ld = "/data/LDScore/Reference_Files/eur_w_ld_chr/"
+    )
+
+# Load files paths --------------------------------------------------------
+
+gwas_details <- 
+  tibble(
+    full_path = list.files(path = gwas_dir, 
+                           recursive = T, 
+                           full.names = T,
+                           pattern = ".sumstats.gz")) %>% 
+  dplyr::mutate(
+    file_name = basename(full_path) %>% 
+      stringr::str_remove(".sumstats.gz") %>% 
+      stringr::str_replace_all("_", "\\.")) %>% 
+  dplyr::filter(file_name %in% phenotypes)
+
+# Main --------------------------------------------------------------------
+
+###### Running bivariate LDSC ######
+
+# Generate all possible permutations with repetitions of file paths to run against each other
+permutations <-
+  gwas_details %>% 
+  dplyr::mutate(
+    full_path_1 = full_path %>% 
+      as.character(), 
+    full_path_2 = full_path %>% 
+      as.character()
+  ) %>% 
+  tidyr::expand(full_path_1, full_path_2) %>% 
+  dplyr::mutate(
+    phen_1 = 
+      basename(full_path_1) %>% 
+      str_remove("\\..*"),
+    phen_2 = 
+      basename(full_path_2) %>% 
+      str_remove("\\..*")
+  ) %>% 
+  dplyr::arrange(full_path_1)
+
+# Run loop across permutations, with external call to ldsc
+# If many permutations, might consider parallelising process
+for(i in 1:nrow(permutations)){
+  
+  args <- 
+    list(
+      phen_1 = permutations$full_path_1[i],
+      phen_2 = permutations$full_path_2[i],
+      out_prefix = 
+        stringr::str_c(
+          permutations$phen_1[i],
+          "_",
+          permutations$phen_2[i]
+        )
+    )
+  
+  rg_arg <- 
+    stringr::str_c(
+      " ", ldsc_args$ldsc,
+      " --rg ", args$phen_1, ",", args$phen_2,
+      " --ref-ld-chr ", ldsc_args$ref_ld,
+      " --w-ld-chr ", ldsc_args$ref_ld,
+      " --out ", out_dir, "/", args$out_prefix, "_rg"
+    )
+  
+  # Run command
+  system2(command = "python", args = rg_arg)
+  
+}
+
+###### Extracting LDSC results ######
+
+# Extract all outputs into single file
+file_paths <- 
+  list.files(
+    out_dir, 
+    pattern = "_rg.log",
+    full.names = T
+  )
+
+files <- 
+  vector(mode = "list", 
+         length = length(file_paths))
+
+for(i in 1:length(file_paths)){
+  
+  files[[i]] <- 
+    read_delim(
+      file = file_paths[i],
+      skip = 60, # Number of lines to skip in log file 
+      n_max = 1, # Number of lines to read
+      delim = " ", 
+      trim_ws = T)
+  
+}
+
+all_rg <-
+  files %>% 
+  qdapTools::list_df2df(col1 = "list_name") %>% 
+  dplyr::select(-list_name) %>% 
+  dplyr::mutate(
+    p1 = basename(p1) %>% 
+      stringr::str_remove(".sumstats.gz") %>% 
+      stringr::str_replace_all("_", "\\."),
+    p2 = basename(p2) %>% 
+      stringr::str_remove(".sumstats.gz") %>% 
+      stringr::str_replace_all("_", "\\.")
+  )
+
+###### Creating sample overlap matrix ######
+
+n <- length(phenotypes)
+covar_matrix <- matrix(NA,n,n)                    
+rownames(covar_matrix) <- colnames(covar_matrix) <- phenotypes    
+
+for(i in phenotypes) {
+  for(j in phenotypes) {
+    
+    covar_matrix[i,j] <- 
+      all_rg %>% 
+      dplyr::filter(p1 == i, p2 == j) %>% 
+      .[["gcov_int"]]
+    
+  }
+}
+
+# Sometimes there might be small differences in gcov_int depending on which phenotype was analysed as the outcome / predictor
+if (!all(t(covar_matrix)==covar_matrix)) { 
+  covar_matrix[lower.tri(covar_matrix)] <- t(covar_matrix)[lower.tri(covar_matrix)] 
+  }  
+
+# Standardise the matrix
+covar_matrix <-
+  covar_matrix %>% 
+  cov2cor() %>% 
+  round(digits = 5)
+
+# Save data ---------------------------------------------------------------
+
+write_delim(
+  all_rg, 
+  delim = "\t", 
+  file = file.path(out_dir, "ldsc_correlations.txt")
+)
+
+write.table(
+  covar_matrix, 
+  file = file.path(here::here("results", "01_input_prep"),
+                   "sample_overlap.txt"), 
+  quote = F
+)
